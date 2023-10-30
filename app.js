66e3344bfe021625b798734205dd015b626431ba
02danieljonas <02danieljonas@gmail.com>
2023-10-30 01:32:27 -0400

    404s tell you what method and path was requested

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(security.extractUserFromJwt);
 
-
 // routes
 app.get("/", function (req, res) {
     return res.status(200).json({ ping: "pong" });
@@ -23,7 +22,9 @@ app.use("/pet", petRoutes);
 
 /* Handle all 404 errors that weren't matched by a route */
 app.use((req, res, next) => {
-    return next(new NotFoundError("Route Not Found"));
+    return next(
+        new NotFoundError(`Route Not Found: ${req.method} ${req.path}`)
+    );
 });
 
 /* Generic error handler - anything that is unhandled will be handled here */
